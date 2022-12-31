fd9253b25e0951e27d85e1b035b71088dcde724c
iamAnas-zzx <anasahmad9616@gmail.com>
2022-12-31 18:57:44 +0530

    First Commit

---
+++ Personal Portfolio/index.js
@@ -0,0 +1,9 @@
+
+
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+})
+document.querySelector('.cross').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+})
+
